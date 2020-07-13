5c23af75eaee63485e7b0afc8209cc50b21f9212
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-07-13 15:50:16 +0900

    listing user　紐付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_040550) do
+ActiveRecord::Schema.define(version: 2020_07_13_062052) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_07_13_040550) do
     t.string "image_content_type"
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
